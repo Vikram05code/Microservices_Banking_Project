d716d2943956cac036bfba672f541d6d82f7ce46
Vikram <vikky5vikram@gmail.com>
2024-10-22 23:21:03 +0530

    Added Accounts Microservices

---
+++ accounts/src/main/java/com/vikram/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,21 @@
+package com.vikram.accounts.mapper;
+import com.vikram.accounts.entity.Accounts;
+import com.vikram.accounts.dtos.AccountsDto;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
---
+++ accounts/src/main/java/com/vikram/accounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.vikram.accounts.repository;
+
+import com.vikram.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
